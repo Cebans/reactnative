ae3ae73380c8ff63ffafae4fef2584fdf2b4299a
Cebans <julestrodriguez@uniboyaca.edu.co>
2023-10-20 12:33:41 -0500

    commit funcional

--- App.js
+++ App.js
@@ -4,13 +4,14 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import ListaCursos from './pages/ListaCursos';
 import ListaEstudiantes from './pages/ListaEstudiantes';
 import Inicio from './pages/Inicio';
+import 'react-native-gesture-handler';
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Inicio">
+      <Drawer.Navigator>
         <Drawer.Screen name="Inicio" component={Inicio} />
         <Drawer.Screen name="ListaCursos" component={ListaCursos} />
         <Drawer.Screen name="ListaEstudiantes" component={ListaEstudiantes} />
@@ -18,12 +19,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ffff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- pages/Inicio.js
+++ pages/Inicio.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
 
-function Inicio({ navigation }) {
+
+
+export default function Inicio({ navigation }) {
   return (
     <View>
       <Text>Página de Inicio</Text>
@@ -16,5 +18,3 @@ function Inicio({ navigation }) {
     </View>
   );
 }
-
-export default Inicio;
